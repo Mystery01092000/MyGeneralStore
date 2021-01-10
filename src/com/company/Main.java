818879a926260d31911d678941c157342a1f71c3
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-01-10 23:23:55 +0530

    Product structure created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Product p = new Product(1,100000231,"Apple",23.23, 5);
+        System.out.println(p.itemNumber);
+        System.out.println(p.productName);
+        System.out.println(p.productPrice);
     }
 }
--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -2,4 +2,19 @@ package com.company;
 
 public class Product {
 
+    int serialNumber;
+    long itemNumber;
+    String productName;
+    double productPrice;
+    int productQuantity;
+
+    Product(int serialNumber, long itemNumber, String productName, double productPrice, int productQuantity){
+        this.serialNumber = serialNumber;
+        this.itemNumber = itemNumber;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQuantity = productQuantity;
+    }
+
+
 }
