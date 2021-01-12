1b37aacecc8d9c8ac4119d5ac910a72eaf1bceec
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-01-12 21:13:02 +0530

    Main updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,33 @@
 package com.company;
 
+import com.company.purchase.bill;
+
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("WELCOME TO MY GENERAL STORE");
+        System.out.println("CHOOSE A SERVICE");
+        System.out.println("PRESS 1 TO ENTER STORE STORAGE");
+        System.out.println("PRESS 2 FOR USER SERVICE");
+        int option  = sc.nextInt();
 
+        //while(true){
+            if(option == 1){
+                System.out.println("WELCOME TO STORE'S STORAGE");
+
+            }else if(option == 2){
+
+                System.out.println("WELCOME TO THE STORE");
+                System.out.println("GENERATE YOUR BILL HERE");
+                bill b =  new bill();
+                b.bill();
+
+            }
+        //}
     }
 }
--- src/com/company/purchase/bill.java
+++ src/com/company/purchase/bill.java
@@ -12,12 +12,13 @@ public class bill {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the item number followed by Quantity to add to bucket");
         System.out.println("Press Y to add item");
-        String item = sc.next();
+        String item = "y";
         ArrayList<purchase> list = new ArrayList<>();
         int sno=1;
         while(item == "Y" || item == "y"){
             System.out.println("Enter item Number");
             long itemNumber = sc.nextLong();
+
             System.out.println("How much of this you want : ");
             int quantity = sc.nextInt();
 
@@ -25,7 +26,7 @@ public class bill {
             //Add a print statement that will tell what item is purchased and how much and total price
 
             System.out.println("Do you wanna add more item : (press y for yes and any key for no");
-            item  = sc.next();
+
         }
 
 
